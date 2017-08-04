f21f1ba3535aa5b2be28d645c56cb0abb3b1f602
Max Tweddell <maxt.nz@gmail.com>
2017-08-04 14:03:57 +1200

    serach bar working!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import getMovies from './api'
+import SearchBar from './SearchBar'
 
 class App extends Component {
   render() {
@@ -11,9 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Sethman Movie Finder</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.jsx</code> and save to reload.
-        </p>
+        <SearchBar/>
       </div>
     )
   }
