2b5a577f7727dd520a2815fd081f7112cbc5ce17
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-08-04 14:50:29 +1200

    Create movie_list_item.js
    movie_list_item.js file

---
+++ diffway/movie_list_item.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+const MovieLIstItem = ({video}) => {
+ return (
+ <li className='list-group-item'>
+     <div className='video-list media'>
+         <div className='media-left'>
+             <img className='mdeia-object' src={#} />
+             </div>
+             <div className='media-body' >
+                 <div className='media-heading'>{#}</div>
+                 </div>
+                 </div>
+ </li>
+ )
+    //const movie =props.movie
+}
+
+export default MovieLIstItem
