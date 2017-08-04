d5f852b8a7fd5642487e25c9a65d5a0d3fa03426
Max Tweddell <maxt.nz@gmail.com>
2017-08-04 13:58:42 +1200

    internet please

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import getMovies from './api'
 
 class App extends Component {
   render() {
@@ -8,13 +9,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Sethman Movie Finder</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.jsx</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
